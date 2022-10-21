e710ec5c77061dcc73acd911c58be08c36b23ab4
haneul <108397012+haaaaane@users.noreply.github.com>
2022-10-21 18:23:23 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$('.slide-group').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots:false,
+    prevArrow:'<button class="arrleft"><i class="fas fa-angle-left"></i></button>',
+    nextArrow:'<button class="arrright"><i class="fas fa-angle-right"></i></button>'
+});
